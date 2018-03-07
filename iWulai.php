cc9218541858d26339223509fc824d0938db5c97
iWulai <iwulai@qq.com>
2018-03-07 11:22:19 +0800

    iWulai

--- iWulai.php
+++ iWulai.php
@@ -1,4 +1,7 @@
 <?php
-/* +-------------------------------------------------+ */
-/* | Author: iWulai <iwulai@qq.com> <www.iwulai.com> | */
-/* +-------------------------------------------------+ */
+/* +--------------------------------------------------+ */
+/* | Author : iWulai <iwulai@qq.com> <www.iwulai.com> | */
+/* +--------------------------------------------------+ */
+header( 'Content-Type:text/html; charset=utf-8' );
+header( 'X-Powered-By:iWulai' );
+require_once( './iWulai.html' );
