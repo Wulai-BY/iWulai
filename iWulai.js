b775676f7291d3e11e79d541164ccd36953c0c46
iWulai <33288024+Wulai-BY@users.noreply.github.com>
2018-03-28 17:24:14 +0800

    Update iWulai.js

--- iWulai.js
+++ iWulai.js
@@ -2,11 +2,11 @@
 /* | Author : iWulai <iwulai@qq.com> <www.iwulai.com> | */
 /* +--------------------------------------------------+ */
 ( function () {
-    var iWulai = function () {
-
+    var iWulai = function ( name ) {
+        this.name = 'iWulai' || name;
     };
-    iWulai.uploadPicture = function () {
-
+    iWulai.prototype.girlFriend = function () {
+        return 'You have no girl friend! Please wake up!';
     };
     window.iWulai = iWulai;
     return iWulai;
