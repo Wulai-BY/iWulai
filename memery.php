84c3ae09bc342ac7f725aec33ddfe9884b84f835
iWulai <33288024+Wulai-BY@users.noreply.github.com>
2018-06-04 17:25:25 +0800

    Update memery.php

--- memery.php
+++ memery.php
@@ -17,3 +17,22 @@ sudo apt-get install redis-server
 sudo apt-get install git
 sudo apt-get install composer
 composer config -g repo.packagist composer https://packagist.phpcomposer.com
+
+
+/* Ubuntu LNMP 环境搭建 */
+sudo apt-get install nginx
+    /* rewrite moddle */
+    location / {
+        try_files $uri $uri/ /index.php?$query_string;
+    }
+sudo apt-get install php7.2 php7.2-fpm php7.2-mysql php7.2-mbstring php7.2-xml php7.2-json php7.2-curl php7.2-gd
+    /* swoole */
+    sudo apt-get install php7.2-dev php-pear
+    sudo pecl channel-update pecl.php.net
+    sudo apt-get install libpq-dev
+    sudo pecl install swoole
+sudo apt-get install mysql-server
+    /* no password login */     //https://blog.csdn.net/YuYan_wang/article/details/79515940
+    update user set authentication_string=PASSWORD('newpassword') where user='root';
+    update user set plugin='mysql_native_password';
+    flush privileges;
