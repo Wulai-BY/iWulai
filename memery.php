5c94bac6091ae4be2cf75dd609d94aa6cca61400
iWulai <33288024+Wulai-BY@users.noreply.github.com>
2018-06-22 15:50:51 +0800

    Update memery.php

--- memery.php
+++ memery.php
@@ -29,6 +29,9 @@ sudo apt-get install mysql-server
 sudo apt-get install redis-server
 sudo apt-get install git
 sudo apt-get install composer
+sudo apt-get install python-software-properties
+sudo add-apt-repository ppa:ondrej/php
+sudo apt-get update
 composer config -g repo.packagist composer https://packagist.phpcomposer.com
 sudo apt-get install php7.2 php7.2-fpm php7.2-mysql php7.2-mbstring php7.2-xml php7.2-json php7.2-curl php7.2-gd
     /* swoole */
