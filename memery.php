1d280848a64bee80c362b4e335dfc9bbfa5e5514
iWulai <33288024+Wulai-BY@users.noreply.github.com>
2018-07-01 22:58:57 +0800

    Update memery.php

--- memery.php
+++ memery.php
@@ -83,3 +83,5 @@ sudo wget http://www.linuxidc.com/files/repo/google-chrome.list -P /etc/apt/sour
 wget -q -O - https://dl.google.com/linux/linux_signing_key.pub  | sudo apt-key add -
 sudo apt update
 sudo apt install google-chrome-stable
+    
+sudo snap install phpstorm --classic
