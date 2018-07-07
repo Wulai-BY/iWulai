213642d798f382885b2df19bfaf15658be589f75
iWulai <33288024+Wulai-BY@users.noreply.github.com>
2018-07-07 18:17:31 +0800

    Update memery.php

--- memery.php
+++ memery.php
@@ -18,6 +18,24 @@ deb-src http://mirrors.aliyun.com/ubuntu/ trusty-updates main restricted univers
 deb-src http://mirrors.aliyun.com/ubuntu/ trusty-proposed main restricted universe multiverse
 deb-src http://mirrors.aliyun.com/ubuntu/ trusty-backports main restricted universe multiverse
 
+deb http://mirrors.cloud.aliyuncs.com/ubuntu/ xenial main
+deb-src http://mirrors.cloud.aliyuncs.com/ubuntu/ xenial main
+
+deb http://mirrors.cloud.aliyuncs.com/ubuntu/ xenial-updates main
+deb-src http://mirrors.cloud.aliyuncs.com/ubuntu/ xenial-updates main
+
+deb http://mirrors.cloud.aliyuncs.com/ubuntu/ xenial universe
+deb-src http://mirrors.cloud.aliyuncs.com/ubuntu/ xenial universe
+
+deb http://mirrors.cloud.aliyuncs.com/ubuntu/ xenial-updates universe
+deb-src http://mirrors.cloud.aliyuncs.com/ubuntu/ xenial-updates universe
+
+deb http://mirrors.cloud.aliyuncs.com/ubuntu xenial-security main
+deb-src http://mirrors.cloud.aliyuncs.com/ubuntu xenial-security main
+
+deb http://mirrors.cloud.aliyuncs.com/ubuntu xenial-security universe
+deb-src http://mirrors.cloud.aliyuncs.com/ubuntu xenial-security universe
+
 /* Ubuntu LNMP 环境搭建 */
 sudo apt-get install nginx
     /* rewrite moddle */
